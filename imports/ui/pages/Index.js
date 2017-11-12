f991eabc2217776324ad1b9e0aae1a1c07cb90f6
Naruepanat <cstaytay@hotmail.com>
2017-11-12 22:33:30 +0700

    update shopping ui
    fix search bar ui

--- imports/ui/pages/Index.js
+++ imports/ui/pages/Index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
 
+
 const Index = () => (
   <div className="Index">
     <Jumbotron className="text-center">
--- imports/ui/pages/Shopping.js
+++ imports/ui/pages/Shopping.js
@@ -6,50 +6,53 @@ import ShopsList from '../containers/ShopsList.js';
 const Shops = ({ history }) => (
   <div className="Documents">
     <Row>
-      <Col xs={ 12 }>
         <div className="page-header clearfix">
-          <h4 className="pull-left">Shopping</h4>
-        </div>
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={ 3 }>
-      <FormGroup controlId="formControlsSelect">
-        <ControlLabel>Sort by</ControlLabel>
-          <FormControl componentClass="select" name="sort" placeholder="date">
-            <option value="date">date</option>
-            <option value="location">location</option>
-            <option value="price">price</option>
-            <option value="popular">popular</option>
-          </FormControl>
-        </FormGroup>
-      </Col>
-      <Col xs={ 3 }>
-      <FormGroup controlId="formControlsSelect">
-        <ControlLabel>Filter</ControlLabel>
-          <FormControl componentClass="select" name="filter" placeholder="all">
-            <option value="all">all</option>
-            <option value="dog">dog</option>
-            <option value="cat">cat</option>
-            <option value="other">other</option>
-          </FormControl>
-        </FormGroup>
-      </Col>
-      <Col xs={ 3 }>
-          <FormGroup>
-          <ControlLabel>Search</ControlLabel>
-            <InputGroup>
-              <FormControl type="text" />
-              <InputGroup.Addon>
-                <Glyphicon glyph="fa fa-search" />
-              </InputGroup.Addon>
-            </InputGroup>
-          </FormGroup>
-      </Col>
-    </Row>
-    <Row>
-        <ShopsList history={history} />
-    </Row>
+          <Col xs={ 2 } >
+            <h4 >Shopping</h4>
+          </Col>
+          <Col xs={ 2 } xsOffset={1}>
+            <FormGroup controlId="formControlsSelect">
+                <InputGroup>
+                    <FormControl componentClass="select" name="sort" placeholder="date">
+                      <option value="date">date</option>
+                      <option value="location">location</option>
+                      <option value="price">price</option>
+                      <option value="popular">popular</option>
+                    </FormControl>
+                    <InputGroup.Addon>Sort by</InputGroup.Addon>
+                </InputGroup>
+              </FormGroup>
+            </Col>
+            <Col xs={ 2 }>
+              <FormGroup controlId="formControlsSelect">
+                  <InputGroup>
+                    <FormControl componentClass="select" name="filter" placeholder="all">
+                      <option value="all">all</option>
+                      <option value="dog">dog</option>
+                      <option value="cat">cat</option>
+                      <option value="other">other</option>
+                    </FormControl>
+                    <InputGroup.Addon>Filter</InputGroup.Addon>
+                  </InputGroup>
+                </FormGroup>
+              </Col>
+              <Col xs={ 5 }>
+                <FormGroup>
+                <InputGroup>
+                    <FormControl type="text" />
+                    <InputGroup.Button>
+                      <Button>Search</Button>
+                    </InputGroup.Button>
+                  </InputGroup>
+                </FormGroup>
+              </Col>
+          </div>
+      </Row>
+      <Row>
+        <Col xs={ 12 }>
+          <ShopsList history={history} />
+        </Col>
+      </Row>
   </div>
 );
 
