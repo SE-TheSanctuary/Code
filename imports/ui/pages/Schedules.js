1b76759d18fc83e97ce9065930b98dd3fc4bb842
Naruepanat <cstaytay@hotmail.com>
2017-12-08 00:18:04 +0700

    add day packer

--- imports/ui/pages/Schedules.js
+++ imports/ui/pages/Schedules.js
@@ -2,6 +2,9 @@ import React, { PropTypes } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Button } from 'react-bootstrap';
 import BookingsList from '../containers/SchedulesList.js';
+import DayPicker from 'react-day-picker';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
 
 const Bookings = ({ history }) => (
   <div className="Documents">
@@ -11,6 +14,8 @@ const Bookings = ({ history }) => (
           <h4 className="pull-left">My schedules</h4>
 
         </div>
+        <p>Please type a day:</p>
+        <DayPickerInput onDayChange={day => console.log(day)} />
         <BookingsList history={history} />
       </Col>
     </Row>
